67350e33d4e60b98c68e2e7dcb968d2eb5601350
Marco Marassi <thtg88@users.noreply.github.com>
2019-10-22 10:59:25 +0100

    Add beta notice to class's docblock if not a schema.org model.

--- src/generator.js
+++ src/generator.js
@@ -654,6 +654,7 @@ class Generator {
 
     let docLines = [
       this.getPropNameFromFQP(model.type) !== model.type &&
+        model.type.indexOf("schema:") !== 0 &&
         `[NOTICE: This is a beta class, and is highly likely to change in future versions of this library.].`,
       this.createCommentFromDescription(model.description)
     ];
