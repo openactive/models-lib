480a37baf8571582d7714cd3124e751a647baabf
Joe Carter <joe@simpleweb.co.uk>
2019-10-31 11:19:30 +0000

    Fix new bug from adding pending schema (3DModel isn't a valid Ruby class name)

--- src/generators/ruby/index.js
+++ src/generators/ruby/index.js
@@ -143,6 +143,9 @@ class Ruby extends Generator {
   }
 
   convertToClassName(value) {
+    // 3DModel is an invalid class name..
+    value = value.replace(/^3/, 'Three');
+
     return this.convertToCamelCase(value);
   }
 
