4b7b7adf7332947886b926437fbba12638d14abb
Luke Winship <luke.winship@gmail.com>
2021-05-19 14:54:50 +0100

    example file

---
+++ tmp/src/examples/e.ts
@@ -0,0 +1,14 @@
+import { strict as assert } from 'assert';
+import { Event } from '../oa';
+import { OaValidationError } from '../oaValidationError';
+
+console.log('hi0');
+
+const event = Event.validate({ '@type': 'Event' });
+assert(!(event instanceof OaValidationError));
+
+
+// const t: {
+//   x: never;
+//   y: 3;
+// } = { y: 3 }
